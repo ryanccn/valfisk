bc858caae6ae8ab6c41dd77d747d62d9ad180ccb
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-10-17 08:22:16 +0800

    fix: return correct pomelo statistics

--- src/commands/pomelo.rs
+++ src/commands/pomelo.rs
@@ -19,33 +19,34 @@ pub async fn pomelo(ctx: Context<'_>) -> Result<()> {
         let mut nonmigrated_users: Vec<&serenity::UserId> = vec![];
 
         for member in &members {
-            if member.user.discriminator != std::num::NonZeroU16::new(0) {
+            if member.user.discriminator.is_some() {
                 nonmigrated_users.push(&member.user.id);
             };
         }
 
-        ctx.send(
-            CreateReply::new().embed(
-                serenity::CreateEmbed::new()
-                    .title("Username migration / Pomelo")
-                    .description(format!(
-                        "**{}/{}** migrated",
-                        nonmigrated_users.len(),
-                        members.len()
-                    ))
-                    .field(
-                        "Unmigrated users",
-                        nonmigrated_users
-                            .into_iter()
-                            .map(|u| format!("<@{u}>"))
-                            .collect::<Vec<String>>()
-                            .join(" "),
-                        false,
-                    )
-                    .color(0x2dd4bf),
-            ),
-        )
-        .await?;
+        let embed = serenity::CreateEmbed::new()
+            .title("Username migration / Pomelo")
+            .description(format!(
+                "**{}/{}** migrated",
+                members.len() - nonmigrated_users.len(),
+                members.len(),
+            ))
+            .color(0x2dd4bf)
+            .field(
+                "Unmigrated users",
+                if nonmigrated_users.is_empty() {
+                    "None!".to_owned()
+                } else {
+                    nonmigrated_users
+                        .into_iter()
+                        .map(|u| format!("<@{u}>"))
+                        .collect::<Vec<String>>()
+                        .join(" ")
+                },
+                false,
+            );
+
+        ctx.send(CreateReply::new().embed(embed)).await?;
     } else {
         ctx.say("Guild unavailable").await?;
     }
