123e444eb40b4cbfcfa3edab3d3c1b926f39a978
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-10-18 23:04:13 +0800

    perf: parallelize message handlers

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -4,7 +4,7 @@ use poise::serenity_prelude as serenity;
 mod github_expansion;
 
 pub async fn handle(message: &serenity::Message, ctx: &serenity::Context) -> Result<()> {
-    github_expansion::handle(message, ctx).await?;
+    tokio::try_join!(github_expansion::handle(message, ctx))?;
 
     Ok(())
 }
