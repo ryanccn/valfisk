ba5c46639d12d16158d522cd27b43b3defbc4492
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2024-01-12 10:35:53 +0800

    feat(ping): add actual latency

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -5,7 +5,8 @@ use crate::Context;
 /// Pong!
 #[poise::command(slash_command, guild_only)]
 pub async fn ping(ctx: Context<'_>) -> Result<()> {
-    ctx.say("Pong!").await?;
+    let ping = humantime::format_duration(ctx.ping().await);
+    ctx.say(format!("Pong! `{ping}`")).await?;
 
     Ok(())
 }
