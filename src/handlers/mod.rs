2bcc49b3a0af3518bcf334318fb191dac60525e9
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2024-01-17 21:40:57 +0800

    feat: moderation features

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -3,11 +3,21 @@ use poise::serenity_prelude as serenity;
 
 mod code_expansion;
 mod error_handling;
+pub mod log;
 
 pub use error_handling::handle_error;
 
-pub async fn handle_message(message: &serenity::Message, ctx: &serenity::Context) -> Result<()> {
-    tokio::try_join!(code_expansion::handle(message, ctx))?;
+use crate::Data;
+
+pub async fn handle_message(
+    message: &serenity::Message,
+    ctx: &serenity::Context,
+    data: &Data,
+) -> Result<()> {
+    tokio::try_join!(
+        code_expansion::handle(message, ctx),
+        log::handle_message(message, data)
+    )?;
 
     Ok(())
 }
