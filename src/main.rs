007bd49e2614bbec77eec385a2f870699bfa7abb
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2024-01-28 13:44:43 +0800

    refactor: split up main function

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,10 @@
 #![allow(clippy::unreadable_literal, clippy::module_name_repetitions)]
 #![forbid(unsafe_code)]
 
-use color_eyre::eyre::{Context as EyreContext, Error, Result};
+use color_eyre::eyre::{Context as _, Report, Result};
 use log::{error, info, warn};
 
-use poise::{
-    serenity_prelude::{Client, FullEvent, GatewayIntents},
-    Framework, FrameworkOptions,
-};
+use poise::{serenity_prelude as serenity, Framework, FrameworkOptions};
 use storage::Storage;
 
 use crate::utils::Pluralize;
@@ -17,7 +14,7 @@ pub struct Data {
     storage: Option<Storage>,
 }
 
-pub type Context<'a> = poise::Context<'a, Data, Error>;
+pub type Context<'a> = poise::Context<'a, Data, Report>;
 
 mod api;
 mod commands;
@@ -28,7 +25,91 @@ mod storage;
 mod template_channel;
 mod utils;
 
-#[allow(clippy::too_many_lines)]
+async fn event_handler(
+    ctx: &serenity::Context,
+    ev: &serenity::FullEvent,
+    _framework: poise::FrameworkContext<'_, Data, Report>,
+    data: &Data,
+) -> Result<()> {
+    use serenity::FullEvent;
+
+    match ev {
+        FullEvent::Message { new_message } => {
+            handlers::handle_message(new_message, ctx).await?;
+        }
+
+        FullEvent::ReactionAdd { add_reaction } => {
+            let message = add_reaction.message(&ctx).await?;
+            starboard::handle(ctx, data, &message).await?;
+        }
+
+        FullEvent::ReactionRemove { removed_reaction } => {
+            let message = removed_reaction.message(&ctx).await?;
+            starboard::handle(ctx, data, &message).await?;
+        }
+
+        FullEvent::ReactionRemoveAll {
+            removed_from_message_id,
+            channel_id,
+        } => {
+            let message = channel_id.message(&ctx, removed_from_message_id).await?;
+            starboard::handle(ctx, data, &message).await?;
+        }
+
+        FullEvent::ReactionRemoveEmoji { removed_reactions } => {
+            let message = removed_reactions.message(&ctx).await?;
+            starboard::handle(ctx, data, &message).await?;
+        }
+
+        FullEvent::PresenceUpdate { new_data, .. } => {
+            if new_data.guild_id.map(|g| g.to_string()) == std::env::var("GUILD_ID").ok() {
+                let mut store = api::PRESENCE_STORE.write().unwrap();
+                store.insert(
+                    new_data.user.id,
+                    api::ValfiskPresenceData::from_presence(new_data),
+                );
+                drop(store);
+            }
+        }
+
+        &_ => {}
+    }
+
+    Ok(())
+}
+
+async fn setup(
+    ctx: &serenity::Context,
+    ready: &serenity::Ready,
+    framework: &poise::Framework<Data, Report>,
+) -> Result<Data> {
+    info!("Connected to Discord as {}", ready.user.tag());
+
+    let commands = &framework.options().commands;
+
+    poise::builtins::register_globally(&ctx, commands).await?;
+    info!(
+        "Registered {} {}",
+        commands.len(),
+        "command".pluralize(commands.len())
+    );
+
+    if let Ok(redis_url) = std::env::var("REDIS_URL") {
+        let client = redis::Client::open(redis_url)?;
+        let storage = Storage::from(client);
+
+        if let Err(err) = commands::restore_presence(ctx, &storage).await {
+            error!("{err}");
+        };
+
+        Ok(Data {
+            storage: Some(storage),
+        })
+    } else {
+        Ok(Data { storage: None })
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     if std::env::var("RUST_LOG").is_err() {
@@ -51,59 +132,12 @@ async fn main() -> Result<()> {
     let token = std::env::var("DISCORD_TOKEN")
         .context("Could not obtain DISCORD_TOKEN from environment!")?;
 
-    let mut client = Client::builder(token, GatewayIntents::all())
+    let mut client = serenity::Client::builder(token, serenity::GatewayIntents::all())
         .framework(Framework::new(
             FrameworkOptions {
                 commands: commands::to_vec(),
-                event_handler: |ctx, ev, _, data| {
-                    Box::pin(async move {
-                        match ev {
-                            FullEvent::Message { new_message } => {
-                                handlers::handle_message(new_message, ctx).await?;
-                            }
-
-                            FullEvent::ReactionAdd { add_reaction } => {
-                                let message = add_reaction.message(&ctx).await?;
-                                starboard::handle(ctx, data, &message).await?;
-                            }
-
-                            FullEvent::ReactionRemove { removed_reaction } => {
-                                let message = removed_reaction.message(&ctx).await?;
-                                starboard::handle(ctx, data, &message).await?;
-                            }
-
-                            FullEvent::ReactionRemoveAll {
-                                removed_from_message_id,
-                                channel_id,
-                            } => {
-                                let message =
-                                    channel_id.message(&ctx, removed_from_message_id).await?;
-                                starboard::handle(ctx, data, &message).await?;
-                            }
-
-                            FullEvent::ReactionRemoveEmoji { removed_reactions } => {
-                                let message = removed_reactions.message(&ctx).await?;
-                                starboard::handle(ctx, data, &message).await?;
-                            }
-
-                            FullEvent::PresenceUpdate { new_data, .. } => {
-                                if new_data.guild_id.map(|g| g.to_string())
-                                    == std::env::var("GUILD_ID").ok()
-                                {
-                                    let mut store = api::PRESENCE_STORE.write().unwrap();
-                                    store.insert(
-                                        new_data.user.id,
-                                        api::ValfiskPresenceData::from_presence(new_data),
-                                    );
-                                    drop(store);
-                                }
-                            }
-
-                            &_ => {}
-                        }
-
-                        Ok(())
-                    })
+                event_handler: |ctx, ev, framework, data| {
+                    Box::pin(event_handler(ctx, ev, framework, data))
                 },
                 on_error: |err| {
                     Box::pin(async move {
@@ -112,35 +146,7 @@ async fn main() -> Result<()> {
                 },
                 ..Default::default()
             },
-            |ctx, ready, framework| {
-                Box::pin(async move {
-                    info!("Connected to Discord as {}", ready.user.tag());
-
-                    let commands = &framework.options().commands;
-
-                    poise::builtins::register_globally(&ctx, commands).await?;
-                    info!(
-                        "Registered {} {}",
-                        commands.len(),
-                        "command".pluralize(commands.len())
-                    );
-
-                    if let Ok(redis_url) = std::env::var("REDIS_URL") {
-                        let client = redis::Client::open(redis_url)?;
-                        let storage = Storage::from(client);
-
-                        if let Err(err) = commands::restore_presence(ctx, &storage).await {
-                            error!("{err}");
-                        };
-
-                        Ok(Data {
-                            storage: Some(storage),
-                        })
-                    } else {
-                        Ok(Data { storage: None })
-                    }
-                })
-            },
+            |ctx, ready, framework| Box::pin(setup(ctx, ready, framework)),
         ))
         .await?;
 
